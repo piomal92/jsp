d82d4999e034861157865b7c9ec7a10b39c56d1d
piotr <piomal3@gmail.com>
2020-08-23 10:59:47 +0200

    zadanie 21

--- src/main/java/pl/sda/servlet/HelloWorldServlet.java
+++ src/main/java/pl/sda/servlet/HelloWorldServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet
+@WebServlet (name="HelloWorldServlet", urlPatterns="/")
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
